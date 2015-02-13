41eca615756630c4fe071c62fa47c4a053d7979c
Alex Urquhart <alexurquhart@gmail.com>
2015-02-13 14:56:33 -0500

    updated website

--- website/www/src/js/App.ts
+++ website/www/src/js/App.ts
@@ -1,6 +1,8 @@
 /// <reference path="types/knockout/knockout.d.ts"/>
+
 /// <reference path="Feed.ts"/>
 /// <reference path="Map.ts"/>
+/// <reference path="ViewModel.ts"/>
 
 var map: TweetMap = new TweetMap;
 
--- website/www/src/js/ViewModel.ts
+++ website/www/src/js/ViewModel.ts
@@ -1,1 +1,21 @@
-class ViewModel {}
+/// <reference path="types/knockout/knockout.d.ts"/>
+/// <reference path="Tweet.ts"/>
+
+class ViewModel {
+	private onlineCount: KnockoutObservable<number>;
+	private latestTweets: KnockoutObservableArray<Tweet>;
+	private latestPictures: KnockoutComputedStatic;
+
+	constructor() {
+		this.onlineCount = ko.observable(1);
+		this.latestTweets = ko.observableArray([]);
+		this.latestPictures = ko.computed()
+		this.
+
+		console.log(this.latestTweets());
+	}
+
+	getLatestPictures(): KnockoutComputedStatic {
+
+	}
+}
